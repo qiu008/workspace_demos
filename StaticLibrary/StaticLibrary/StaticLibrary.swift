58bc377ee9e9a72c5ac8f3dadc4db705a6307625
qiuyh108 <49462494+qiuyh108@users.noreply.github.com>
2024-11-17 16:43:28 +0800

    Add files via upload

---
+++ StaticLibrary/StaticLibrary/StaticLibrary.swift
@@ -0,0 +1,12 @@
+//
+//  StaticLibrary.swift
+//  StaticLibrary
+//
+//  Created by STL_QYH on 2021/8/13.
+//
+
+public class StaticLibrary {
+    public func testFunc() {
+        print("testFunc")
+    }
+}
